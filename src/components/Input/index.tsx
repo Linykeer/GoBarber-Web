474ddfe91dd88223cf5b2029662d43e6b5e4a62e
Linykeer Almeida <linykeerrenato@gmail.com>
2020-09-12 14:37:05 -0300

    Erro na estilização do input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,31 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useRef, useEffect, useState } from 'react';
 import { Container } from './styles';
+import { IconBaseProps } from 'react-icons/lib';
+import { useField } from '@unform/core'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = (props) => (
-  <Container>
-    <input {...props} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+  const [isFocused, setIsFocused] = useState(false)
+
+  useEffect(() => { }, [fieldName, registerField])
+
+  return (
+    < Container isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container >
+  )
+}
 export default Input;
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import logoImg from '../../assets/sign-in-background.png'
 import logo from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles';
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logo} alt="GoBarber" />
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
+        <Input name="name" placeholder="Nome" />
         <Input name="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
         <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
       </form>
       <a href="">
-        <FiLogIn />
-        Criar Conta
+        <FiArrowLeft />
+       Voltar para login
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
